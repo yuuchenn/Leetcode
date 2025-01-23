ec6443ca8ec6af59895d78872bc9fecd7962bd7a
Niche. <60465450+yuuchenn@users.noreply.github.com>
2025-01-23 20:11:11 +0800

    Delete LeetCode75/1045_Customers_Who_Bought_All_Products.sql

--- LeetCode75/1045_Customers_Who_Bought_All_Products.sql
+++
@@ -1,8 +0,0 @@
-
-select a.customer_id
-  from Customer a, Product b
--- owned product in Product table
- where a.product_key = b.product_key
- group by a.customer_id
--- product nums = total nums of product in Product table
-having count(distinct a.product_key) = (select count(*) from Product)
