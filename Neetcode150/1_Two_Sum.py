60f67b07dda39c3b514693ffc8fa9649b7f68efd
Niche. <60465450+yuuchenn@users.noreply.github.com>
2025-02-07 22:27:45 +0800

    Update 1_Two_Sum.py

--- Neetcode150/1_Two_Sum.py
+++ Neetcode150/1_Two_Sum.py
@@ -1,4 +1,15 @@
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = {}
+        for i,n in enumerate(nums):
+            if diff.get(n) is not None:
+                return [diff.get(n),i]
+            diff[target-n] = i
+            
+  # time complexity: O(N)
+  # space complexity: O(N)
+
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
         remains = dict()
         for i in range(len(nums)):
       
